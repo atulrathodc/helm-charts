2333c70d63eb0b41bb1c2134837cd2ec7351f490
Atul Rathod <atulrathodc@gmail.com>
2026-02-13 09:29:50 +0530

    updating charts

--- git.sh
+++ git.sh
@@ -1,3 +1,10 @@
+helm repo index . \
+--url https://atulrathodc.github.io/helm-charts
+
+helm repo index . \
+--url https://atulrathodc.github.io/helm-charts \
+--merge index.yaml
+
 #!/bin/bash
 
 COMMIT_MESSAGE="updating charts"
